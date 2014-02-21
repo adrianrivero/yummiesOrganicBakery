c6961974f5e775f2d31460bed1a2364b0f0f9a63
adrianrivero <hectikrivero@gmail.com>
2014-02-20 20:03:55 -0500

    Yummies Week 3 / Week 1 of Development

---
+++ app/assets/js/config.js
@@ -0,0 +1,35 @@
+var app = angular.module('YummiesApp', ['ngRoute','firebase'])
+
+	.config(['$routeProvider', function (routes) {
+
+		//Routes
+		routes
+			// Main Page
+			.when('/', {
+				templateUrl: 'assets/views/main.yum',
+			})
+			//About Us
+			.when('/aboutus', {
+				templateUrl: 'assets/views/aboutus.yum',
+				//controller: 'SignInController'
+			})
+
+			//Shop
+			.when('/shop', {
+				templateUrl: 'assets/views/shop.yum',
+				//controller: 'SignUpController'
+			})
+
+			//Contact 
+			.when('/contact',{
+				templateUrl: 'assets/views/contact.yum',
+			})
+
+			//Products
+			.when('/sweets',{
+				templateUrl: 'assets/views/sweets.yum',
+			})
+
+			.otherwise({redirectTo:'/'});
+
+	}]);
