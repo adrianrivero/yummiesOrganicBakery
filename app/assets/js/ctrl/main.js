ad45ea2a78e46ddea45c49ab6c81c7e47996fb2b
adrianrivero <hectikrivero@gmail.com>
2014-03-20 13:19:39 -0400

    updates

--- app/assets/js/ctrl/main.js
+++ app/assets/js/ctrl/main.js
@@ -6,3 +6,11 @@ app.controller('StoreController',['$scope','$rootScope','$http','$firebase','$lo
 
 }]);
 
+app.controller('AdminController',['$scope','$rootScope','$http','$firebase','$location',function($scope,$rootScope,$http,$firebase,$location){
+
+	var ref = new Firebase('https://yummies-products.firebaseIO.com/');
+
+	
+
+}]);
+
