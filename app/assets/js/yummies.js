d75736cf4ed34e4a5d7025c526fc9d39f5a70d3f
adrianrivero <hectikrivero@gmail.com>
2014-03-20 20:05:12 -0400

    clean up

--- app/assets/js/yummies.js
+++ app/assets/js/yummies.js
@@ -37,14 +37,6 @@ function contactForm(){
 	alert('Thanks!');
 };
 
-function toggle_visibility(id) {
-	var e = document.getElementById(id);
-	if(e.style.display == 'block')
-	e.style.display = 'none';
-	else
-	e.style.display = 'block';
-}
-
 function init(){
 
 	// Sweets Tabs
